959cb42bc03bd15a26080dc27cdb69cefb5137bd
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-07-06 20:11:20 +0600

    data pass korsi

--- src/App.js
+++ src/App.js
@@ -2,36 +2,60 @@ import './App.css';
 
 function App() {
 
+  const products = [
+    {name: "Photoshop", price: "$90.67"},
+    {name: "Illustrator", price: "$60.99"},
+    {name: "PDF Reader", price: "$6.99"}
+  ]
+
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name = "Md. Sabbir Hosen" job = "Software Engineer" company = "Amazon" ></Person>
+      <Products product = {products[0]} ></Products>
+      <Products product = {products[1]} ></Products>
+      <Products product = {products[2]} ></Products>
     </div>
   );
 }
 
-function Person() {
+function Person(props) {
   let personStyle = {
     border: "2px solid green",
     padding: "5px",
-    margin: "10px",
+    margin: "10px 25% 10px 25%",
     backgroundColor: "yellow",
-    width: "40%"
+    width: "50%",
   }
-  const person = {
-    name: "Md. Sabbir Hosen",
-    job: "Software Engineer",
-    company: "Amazon"
-  }
+
   return (
     <div className="person-box" style = {personStyle}>
-      <h1 className="person-name"> {person.name} </h1>
-      <h2 className="person-job"> {person.job} </h2>
-      <h2 className="company-name"> {person.company} </h2>
+      <h1 className="person-name"> {props.name} </h1>
+      <h2 className="person-job"> {props.job} </h2>
+      <h2 className="company-name"> {props.company} </h2>
     </div>
   );
+}
+
+function Products(props) {
+
+  let productStyle = {
+    border: "2px solid gray",
+    margin: "10px",
+    padding: "15px",
+    height: "10%",
+    width: "25%",
+    float: "left",
+    borderRadius: "10px",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    backgroundColor: "#f0ebf2"
+  }
+  return (
+    <div className="products-container" style = {productStyle}>
+      <h2>{props.product.name}</h2>
+      <h4>Price: {props.product.price}</h4>
+      <button className= "product-button" style = {{backgroundColor: "#58aee0", height: "35px", width: "90px", border: "1px", borderRadius: "5px"}}>Buy Now</button>
+    </div>
+  )
 }
 
 export default App;
