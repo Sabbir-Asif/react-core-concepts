6625718fd9a198e2e04114c82a3aa86de1917733
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-07-06 15:49:19 +0600

    component banate pari

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+    </div>
+  );
+}
+
+function Person() {
+  let personStyle = {
+    border: "2px solid green",
+    padding: "5px",
+    margin: "10px",
+    backgroundColor: "yellow",
+    width: "40%"
+  }
+  const person = {
+    name: "Md. Sabbir Hosen",
+    job: "Software Engineer",
+    company: "Amazon"
+  }
+  return (
+    <div className="person-box" style = {personStyle}>
+      <h1 className="person-name"> {person.name} </h1>
+      <h2 className="person-job"> {person.job} </h2>
+      <h2 className="company-name"> {person.company} </h2>
     </div>
   );
 }
