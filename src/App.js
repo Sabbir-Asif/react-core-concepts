d245d45940b8b7b8f7898797458c059951ec400f
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-07-07 01:15:20 +0600

    useSate dekhlam

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Person name = "Md. Sabbir Hosen" job = "Software Engineer" company = "Amazon" ></Person>
+      <Counter></Counter>
       {
         products.map(product => <Products product = {product}></Products>)
       }
@@ -56,6 +58,40 @@ function Products(props) {
       <button className= "product-button" style = {{backgroundColor: "#58aee0", height: "35px", width: "90px", border: "1px", borderRadius: "5px"}}>Buy Now</button>
     </div>
   )
+}
+
+function Counter () {
+  const [count, setCount] = useState(10);
+  let IncreaseButtonStyle = {
+    height: "40px",
+    width: "100px",
+    backgroundColor: "#4cf5ae",
+    color: "black",
+    margin: "10px",
+    padding: "5px",
+    border: "1px solid black",
+    borderRadius: "10px",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  }
+
+  let DecreaseButtonStyle = {
+    height: "40px",
+    width: "100px",
+    backgroundColor: "#fc609c",
+    color: "black",
+    margin: "10px",
+    padding: "5px",
+    border: "1px solid black",
+    borderRadius: "10px",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  }
+  return (
+    <div className="counter-container">
+      <h1>Count: {count} </h1>
+      <button className="increase-button" style = {IncreaseButtonStyle} onClick = {() => setCount(count + 1)}>Increase</button>
+      <button className="decrease-button" style = {DecreaseButtonStyle} onClick = {() => setCount(count - 1)}>Decrease</button>
+    </div>
+  );
 }
 
 export default App;
